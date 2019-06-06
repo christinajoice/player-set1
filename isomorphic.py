a730835183d19731fbc87b62bbf4d693dd9ad03c
christinajoice <christinallrejoice@gmail.com>
2019-06-06 10:47:27 +0530

    Update isomorphic.py

--- isomorphic.py
+++ isomorphic.py
@@ -1,10 +1,7 @@
-str1=input("Enter string 1")
-str2=input("Enter string 2")
-if len(str1)==len(str2):
-  for i in range(0,len(str1)):
-    for j in range(i+1,len(str1)):
-      if str1[i]==str1[j]:
-        temp=str1[i+1]
-        str[i+1]=str[j]
-        str[j]=temp
-print(str1)
+s1,s2=list(map(str,input().split()))
+d1=dict(zip(s1,s2))
+d2=dict(zip(s2,s1))
+if len(d1)==len(d2):
+  print("yes")
+else:
+  print("no")
