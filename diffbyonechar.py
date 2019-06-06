f34f543629280121c7b6906bdd89ed77dfa90266
christinajoice <christinallrejoice@gmail.com>
2019-06-06 10:18:00 +0530

    Create diffbyonechar.py

---
+++ diffbyonechar.py
@@ -0,0 +1,10 @@
+s1,s2=map(str,input().split())
+count=0
+if len(s1)==len(s2):
+  for i in range(len(s1)):
+    if s1[i]!=s2[i]:
+      count=count+1
+if count==1:
+  print("yes")
+else:
+  print("no")
